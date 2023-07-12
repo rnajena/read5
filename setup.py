28b1310194f4768c88b5a3a0089bc5d41979556b
Jannes Spangenberg <janspang@gmx.de>
2023-07-12 13:28:14 +0200

    Make pip wheel work

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
 requirements = [
     # package requirements go here
     'h5py>=3.0',
-    'ont_vbz_hdf_plugin>=1.0',
+    # 'ont_vbz_hdf_plugin>=1.0',
+    'vbz-h5py-plugin>=1.0', # same as ont_vbz but in pipy
     'pyslow5>=1.0',
     'numpy',
-    # 'pod5' # currently only avaible via pipy
+    'pod5', # currently only avaible via pipy
 ]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='read5',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Wrapper to read fast5, slow5, blow5 and pod5 files.",
-    long_description="Read5 is a python wrapper to read fast5, slow5/blow5 and pod5 files using the same overloaded functions from different APIs.",
+    long_description=long_description,
     license="GNU General Public License v3",
     author="Jannes Spangenberg",
     author_email='jannes.spangenberg@uni-jena.de',
     url='https://github.com/JannesSP/read5',
-    packages=['read5'],
+    packages=find_packages(),
     install_requires=requirements,
     keywords=['read5', 'slow5', 'blow5', 'fast5', 'pod5', 'ONT', 'Oxford Nanopore Technologies', 'Nanopore', 'raw data', 'wrapper'],
     classifiers=[
