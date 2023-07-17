f22a8334f42ccba395287acd613d9e41b6003703
Jannes Spangenberg <janspang@gmx.de>
2023-07-17 15:06:35 +0200

    Change from calling setup.py directly to pypa/build

--- pypi.sh
+++ pypi.sh
@@ -1,3 +1,3 @@
-python3 -m pip install --user --upgrade setuptools wheel
-python3 setup.py sdist bdist_wheel
+python3 -m pip install --user --upgrade setuptools wheel build
+python3 -m build
 twine upload dist/*
